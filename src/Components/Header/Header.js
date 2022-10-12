b80826ab62d11acc22be257c8c169b493bf817bb
Auditi <auditibarua6@gmail.com>
2022-10-12 15:22:24 +0300

    added header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,15 +5,13 @@ import './Header.css';
 const Header = () => {
   return (
     <div className='header'>
-      <nav className='nav'>
         <h3>Quizizz</h3>
-        <nav>
+        <nav className='nav'>
           <NavLink to='/'>Home</NavLink>
           <NavLink to='/statistics'>Statistics</NavLink>
           <NavLink to='/blog'>Blog</NavLink>
           <NavLink to='/topic'>Topic</NavLink>
         </nav>
-      </nav>
     </div>
 
   );
